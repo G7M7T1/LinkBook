e45827b72292e7448abe7e7627a92ce96679cc25
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-10-21 18:33:50 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{cshtml,js}",
+    "./LinkBook/Views/*.{cshtml,js}",
+    "./LinkBook/Views/Home/*.{cshtml,js}",
+    "./LinkBook/Views/Shared/*.{cshtml,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
