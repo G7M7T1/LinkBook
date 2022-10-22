310dbc68f4b620344dad1325c9fb7247ef77a113
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-10-21 20:37:40 -0400

    Update Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,10 @@ module.exports = {
   content: [
     "./src/**/*.{cshtml,js}",
     "./LinkBook/Views/*.{cshtml,js}",
+    "./LinkBook/Views/**/*.{cshtml,js}",
     "./LinkBook/Views/Home/*.{cshtml,js}",
-    "./LinkBook/Views/Shared/*.{cshtml,js}"
+    "./LinkBook/Views/Shared/*.{cshtml,js}",
+    "./LinkBook/wwwroot/js/*.{cshtml,js}"
   ],
   theme: {
     extend: {},
