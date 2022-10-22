c6f2ac229d936b683a9b0ca0aedf6b0f57e56e15
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-10-22 07:08:39 -0400

    Update Database

---
+++ LinkBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using LinkBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinkBook.Data;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Category> Categories { get; set; }
+}
