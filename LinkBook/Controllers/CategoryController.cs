88efb683ef0e6ddb8d019a5da28ffee946b2cd77
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-10-23 19:09:35 -0400

    Add File

--- LinkBook/Controllers/CategoryController.cs
+++ LinkBook/Controllers/CategoryController.cs
@@ -43,4 +43,77 @@ public class CategoryController : Controller
         }
         return View(obj);
     }
+    
+    public IActionResult Edit(int? id)
+    {
+        if (id is null or 0)
+        {
+            return NotFound();
+        }
+
+        var categoryFromDb = _db.Categories.Find(id);
+        // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+        // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+
+        return View(categoryFromDb);
+    }
+    
+    //post
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "Name Cannot Match The Display Order");
+        }
+        
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+    
+    // delete get
+    public IActionResult Delete(int? id)
+    {
+        if (id is null or 0)
+        {
+            return NotFound();
+        }
+        
+        var categoryFromDb = _db.Categories.Find(id);
+        // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+        // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+        
+        _db.Categories.Remove(categoryFromDb);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    
+    //post
+    // [HttpPost]
+    // [ValidateAntiForgeryToken]
+    // public IActionResult Delete(int? id)
+    // {
+    //     var obj = _db.Categories.Find(id);
+    //     if (obj == null)
+    //     {
+    //         return NotFound();
+    //     }
+    //     _db.Categories.Remove(obj);
+    //     _db.SaveChanges();
+    //     return RedirectToAction("Index");
+    // }
 }
