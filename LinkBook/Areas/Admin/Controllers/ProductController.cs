42c5ce98a0317bb88b8dcb7c45685e93c44417f3
G7M7T1 <84965845+G7M7T1@users.noreply.github.com>
2022-10-25 12:22:10 -0400

    Update File

--- LinkBook/Areas/Admin/Controllers/ProductController.cs
+++ LinkBook/Areas/Admin/Controllers/ProductController.cs
@@ -20,92 +20,76 @@ public class ProductController : Controller
         IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll();
         return View(objProductList);
     }
-
-    //get
-    // public IActionResult Create()
-    // {
-    //     return View();
-    // }
-    //
-    // //post
-    // [HttpPost]
-    // [ValidateAntiForgeryToken]
-    // public IActionResult Create(CoverType obj)
-    // {
-    //     if (ModelState.IsValid)
-    //     {
-    //         _unitOfWork.CoverType.Add(obj);
-    //         _unitOfWork.Save();
-    //         return RedirectToAction("Index");
-    //     }
-    //     return View(obj);
-    // }
-    //
-    // public IActionResult Edit(int? id)
-    // {
-    //     if (id is null or 0)
-    //     {
-    //         return NotFound();
-    //     }
-    //
-    //     var coverTypeFromDb = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
-    //     // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
-    //     // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
-    //     if (coverTypeFromDb == null)
-    //     {
-    //         return NotFound();
-    //     }
-    //
-    //     return View(coverTypeFromDb);
-    // }
-    //
-    // //post
-    // [HttpPost]
-    // [ValidateAntiForgeryToken]
-    // public IActionResult Edit(CoverType obj)
-    // {
-    //     if (ModelState.IsValid)
-    //     {
-    //         _unitOfWork.CoverType.Update(obj);
-    //         _unitOfWork.Save();
-    //         return RedirectToAction("Index");
-    //     }
-    //     return View(obj);
-    // }
-    //
-    // // delete get
-    // public IActionResult Delete(int? id)
-    // {
-    //     if (id is null or 0)
-    //     {
-    //         return NotFound();
-    //     }
-    //     
-    //     var coverTypeFromDb = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
-    //     // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
-    //     // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
-    //     if (coverTypeFromDb == null)
-    //     {
-    //         return NotFound();
-    //     }
-    //     
-    //     _unitOfWork.CoverType.Remove(coverTypeFromDb);
-    //     _unitOfWork.Save();
-    //     return RedirectToAction("Index");
-    // }
     
-    //post
-    // [HttpPost]
-    // [ValidateAntiForgeryToken]
-    // public IActionResult Delete(int? id)
-    // {
-    //     var obj = _db.Categories.Find(id);
-    //     if (obj == null)
-    //     {
-    //         return NotFound();
-    //     }
-    //     _db.Categories.Remove(obj);
-    //     _db.SaveChanges();
-    //     return RedirectToAction("Index");
-    // }
+    public IActionResult Upsert(int? id)
+    {
+        Product product = new();
+         
+         if (id is null or 0)
+         {
+             return View(product);
+         }
+
+         else
+         {
+             var coverTypeFromDb = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+             // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+             // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+             if (coverTypeFromDb == null)
+             {
+                 return NotFound();
+             }   
+         }
+         return View(product);
+     }
+    
+     //post
+     [HttpPost]
+     [ValidateAntiForgeryToken]
+     public IActionResult Upsert(Product obj)
+     {
+         if (ModelState.IsValid)
+         {
+             _unitOfWork.Product.Update(obj);
+             _unitOfWork.Save();
+             return RedirectToAction("Index");
+         }
+         return View(obj);
+     }
+    
+    //  // delete get
+    //  public IActionResult Delete(int? id)
+    //  {
+    //      if (id is null or 0)
+    //      {
+    //          return NotFound();
+    //      }
+    //      
+    //      var coverTypeFromDb = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+    //      // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+    //      // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+    //      if (coverTypeFromDb == null)
+    //      {
+    //          return NotFound();
+    //      }
+    //      
+    //      _unitOfWork.CoverType.Remove(coverTypeFromDb);
+    //      _unitOfWork.Save();
+    //      return RedirectToAction("Index");
+    //  }
+    //
+    // post
+    //  [HttpPost]
+    //  [ValidateAntiForgeryToken]
+    //  public IActionResult Delete(int? id)
+    //  {
+    //      var obj = _db.Categories.Find(id);
+    //      if (obj == null)
+    //      {
+    //          return NotFound();
+    //      }
+    //      _db.Categories.Remove(obj);
+    //      _db.SaveChanges();
+    //      return RedirectToAction("Index");
+    //  }
 }
